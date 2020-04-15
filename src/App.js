ead921fe3a79b7d36190d3487a4ee820401e89d6
UmmuRasul <ummurasul5@gmail.com>
2020-04-15 10:40:48 +0300

    map and component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 import Joke from './Joke';
 
 function App() {
+  const nums = [1,2,3,4,5,6,7,8,9,10]
+  const double = nums.map(function(num) {
+    return num = 2
+  })
+  console.log(double)
   return (
     <div className="joke">
-    <Joke question="Mariam Abdallah Ali" punchline="Zainab Abdallah Ali"/>
-    <Joke question="Mariam Abdallah Ali" punchline="Zainab Abdallah Ali"/>
-    <Joke question="Mariam Abdallah Ali" punchline="Zainab Abdallah Ali"/>
-    <Joke question="Mariam Abdallah Ali" punchline="Zainab Abdallah Ali"/>
-    <Joke question="Mariam Abdallah Ali" punchline="Zainab Abdallah Ali"/>
+    <Joke punchline="It's hard to explain"/>
+    <Joke question="the things to remember" punchline="It's hard to explain"/>
+    <Joke question="the things to remember" punchline="It's hard to explain"/>
+    <Joke question="the things to remember" punchline="It's hard to explain"/>
+    <Joke question="the things to remember" punchline="It's hard to explain"/>
     </div>
   )
 }
---
+++ src/JokeData.js
@@ -0,0 +1,32 @@
+const jokesData = [
+  {
+    id:1,
+    punchline:"It's hard to explain"
+  },
+  {
+    id:2,
+    question:"the things to remember",
+    punchline:"It's hard to explain"
+  },
+  {
+    id:3,
+    question:"the things to remember",
+    punchline:"It's hard to explain"
+  },
+  {
+    id:4,
+    question:"the things to remember",
+    punchline:"It's hard to explain"
+  },
+  {
+    id:5,
+    question:"the things to remember",
+    punchline:"It's hard to explain"
+  },
+  {
+    id:6,
+    question:"the things to remember",
+    punchline:"It's hard to explain"
+  }
+]
+export default jokesData

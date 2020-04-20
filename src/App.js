72ae33a1c69a0cc196dd002f5c1408aab278c649
UmmuRasul <ummurasul5@gmail.com>
2020-04-20 18:21:26 +0300

    radio buttons

--- src/App.js
+++ src/App.js
@@ -10,24 +10,76 @@ class App extends Component{
     super()
     this.state = {
       firstName:"",
-      lastName:""
+      lastName:"",
+      isFriendly: true,
+      gender:""
     }
     this.handleChange = this.handleChange.bind(this)
-
   }
   handleChange(event){
-    this.setState({
-      firstName: event.target.value
-    })
-
+    const {name, value, type, checked} = event.target
+    type === "checked" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
   }
     render(){
       return(
-      <form>
-        <input type="text" placeholder="First Name" onChange={this.handleChange}/>
-        <br/>
-        <input type="text" placeholder="last Name" onChange={this.handleChange}/>
-        <h1>{this.state.firstName}{this.state.lastName}</h1>
+        <form>
+          <input
+            type="text"
+            value={this.state.firstName}
+            name="firstNmae"
+            placeholder="First Name"
+            onChange={this.handleChange}
+          />
+          <br/>
+          <input
+            type="text"
+            value={this.state.lastName}
+            name="lastNmae"
+            placeholder="last Name"
+            onChange={this.handleChange}
+          />
+        {
+
+        }
+        <br />
+          <textarea
+            value={"some default values"}
+            onChange={this.handleChange}
+            />
+          <br />
+          <label>
+          <input type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          />Is Friendly
+          </label>
+
+          <br />
+          <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.gender === "male"}
+            onChange={this.handleChange}
+          />Male
+          </label>
+
+          <label>
+          <br />
+          <input
+            type="radio"
+            name="gender"
+            value="Female"
+            checked={this.state.gender === "female"}
+            onChange={this.handleChange}
+          />Female
+          </label>
+
+
+          <h1>{this.state.firstName}{this.state.lastName}</h1>
+          <h2>You are a {this.state.gender}</h2>
       </form>
       )
     }

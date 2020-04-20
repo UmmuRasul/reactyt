2945cdedcc7cbc934580530a48be6ce60964c976
UmmuRasul <ummurasul5@gmail.com>
2020-04-20 15:14:54 +0300

    using componentDidMount

--- src/App.js
+++ src/App.js
@@ -5,36 +5,65 @@ import TodoItem from './TodoItem';
 import todosData from './todosData';
 
 
-class App extends React.Component {
+class App extends Component{
   constructor(){
     super()
-    this.state ={
-      todos:todosData
+    this.state = {
+      loading: false,
+      character:{}
     }
-    this.handleChange = this.handleChange.bind(this)
   }
-
-  handleChange(id){
-  this.setState(prevState => {
-    const updatedTodos = prevState.todos.map(todo =>{
-      if(todo.id === id){
-        todo.completed ==!todo.completed
-      }
-      return todo
-    })
-    return{
-      todos: updatedTodos
+    componentDidMount(){
+      this.setState({loading: true})
+      fetch('https://swapo.co/api/people/1')
+        .then(responce => responce.json())
+        .then(data => {
+          this.setState({
+            loading: false,
+            character:data
+        })
+      });
     }
-  })
+    render(){
+      const text = this.state.loading ? "loading...": this.state.character.name
+      return(
+        <div>
+        {text}
+        </div>
+      )
+    }
   }
-  render(){
-  const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-
-  return (
-    <div className='todo-list'>
-      {todoItems}
-    </div>
-    )
-  }
-}
 export default App;
+//  class App extends React.Component {
+//   constructor(){
+//     super()
+//     this.state ={
+//       todos:todosData
+//     }
+//     this.handleChange = this.handleChange.bind(this)
+//   }
+//
+//   handleChange(id){
+//   this.setState(prevState => {
+//     const updatedTodos = prevState.todos.map(todo =>{
+//       if(todo.id === id){
+//         todo.completed = !todo.completed
+//       }
+//       return todo
+//     })
+//     return{
+//       todos: updatedTodos
+//     }
+//   })
+//   }
+//   render(){
+//   const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}
+//   handleChange={this.handleChange}/>)
+//
+//   return (
+//     <div className='todo-list'>
+//       {todoItems}
+//     </div>
+//     )
+//   }
+// }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,19 @@
 import React from "react"
 
 function TodoItem(props){
+  const completestyle = {
+    fontStyle:"italic",
+    color:"#cdcdcd",
+    textDecoration:"line-through"
+  }
   return (
     <div className="todo-item">
       <input
         type="checkbox"
         checked={props.item.completed}
-        onchanged={() => props.handleChange(props.item.id)}/>
-      <p>{props.item.text}</p>
+        onchanged={() => props.handleChange(props.item.id)}
+        />
+      <p style={props.item.completed ? completestyle: null}>{props.item.text}</p>
     </div>
   )
 }

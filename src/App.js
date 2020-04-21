5262a66c8adae849712a2fd08f70e4f67d3fb078
UmmuRasul <ummurasul5@gmail.com>
2020-04-21 16:14:40 +0300

    continuing on state

--- src/App.js
+++ src/App.js
@@ -11,110 +11,127 @@ class App extends Component{
     this.state = {
       firstName:"",
       lastName:"",
-      isFriendly: true,
-      gender:""
+      age: "",
+      gender:"",
+      destination:"",
+        isVegan: false,
+        isKosher:false,
+        isLoctoseFree:false
     }
     this.handleChange = this.handleChange.bind(this)
   }
   handleChange(event){
     const {name, value, type, checked} = event.target
-    type === "checked" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+    type === "checkbox" ?
+      this.setState({
+          [name]: checked
+      })
+    :
+    this.setState({
+      [name]:value
+    })
   }
     render(){
       return(
-        <form>
-          <input
-            type="text"
-            value={this.state.firstName}
-            name="firstNmae"
+      <main>
+          <form>
+            <input
+            name="firstName"
+            value="{this.state.firstName}"
+            onChange={this.handleChange}
             placeholder="First Name"
-            onChange={this.handleChange}
-          />
-          <br/>
-          <input
-            type="text"
-            value={this.state.lastName}
-            name="lastNmae"
-            placeholder="last Name"
-            onChange={this.handleChange}
-          />
-        {
-
-        }
-        <br />
-          <textarea
-            value={"some default values"}
-            onChange={this.handleChange}
             />
+
+          <br/>
+
+          <input
+            name="lastName"
+            value="{this.state.lastName}"
+            onChange={this.handleChange}
+            placeholder="Last Name"
+          />
+
+        <br />
+        <input
+          name="age"
+          value="{this.state.age}"
+          onChange={this.handleChange}
+          placeholder="Age"
+        />
           <br />
           <label>
-          <input type="checkbox"
-            name="isFriendly"
-            checked={this.state.isFriendly}
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={this.stae.gender}
+              onChange={this.handleChange}
+              />Male
+          </label>
+
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={this.stae.gender}
+              onChange={this.handleChange}
+              />Female
+          </label>
+          <br />
+          <select
+            value={this.state.destination}
+            name="destination"
             onChange={this.handleChange}
-          />Is Friendly
+          >
+            <option value="">--Choose destination here--</option>
+            <option value="German">German</option>
+            <option value="Norway">Norway</option>
+            <option value="Northpole">Northpole</option>
+            <option value="Southpole">Southpole</option>
+          </select>
+
+          <br/>
+          <label>
+          <input
+            type="checkbox"
+            name="isVegan"
+            onChange={this.handleChange}
+            checked={this.state.isVegan}
+          />Vegan
           </label>
 
           <br />
           <label>
           <input
-            type="radio"
-            name="gender"
-            value="male"
-            checked={this.state.gender === "male"}
+            type="checkbox"
+            name="isKosher"
             onChange={this.handleChange}
-          />Male
+            checked={this.state.isKosher}
+          />Kosher
           </label>
 
           <label>
-          <br />
           <input
-            type="radio"
-            name="gender"
-            value="Female"
-            checked={this.state.gender === "female"}
+            type="checkbox"
+            name="isKosher"
             onChange={this.handleChange}
-          />Female
+            checked={this.state.isLoctoseFree}
+          />Loctose Free
           </label>
+          <br />
 
-
-          <h1>{this.state.firstName}{this.state.lastName}</h1>
-          <h2>You are a {this.state.gender}</h2>
-      </form>
+          <button>Submit</button>
+          </form>
+          <hr />
+          <h2>Enter Information below:</h2>
+          <p>Your Name: {this.state.firstName}{this.state.lastName}<p/>
+          <p>Your Age: {this.state.age}</p>
+          <p>Your Gender: {this.state.gender}</p>
+          <p>Your Destination: {this.state.destination}</p>
+      </main>
       )
     }
   }
 export default App;
-//  class App extends React.Component {
-//   constructor(){
-//     super()
-//     this.state ={
-//       todos:todosData
-//     }
-//     this.handleChange = this.handleChange.bind(this)
-//   }
-//
-//   handleChange(id){
-//   this.setState(prevState => {
-//     const updatedTodos = prevState.todos.map(todo =>{
-//       if(todo.id === id){
-//         todo.completed = !todo.completed
-//       }
-//       return todo
-//     })
-//     return{
-//       todos: updatedTodos
-//     }
-//   })
-//   }
-//   render(){
-//   const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}
-//   handleChange={this.handleChange}/>)
-//
-//   return (
-//     <div className='todo-list'>
-//       {todoItems}
-//     </div>
-//     )
-//   }
-// }
